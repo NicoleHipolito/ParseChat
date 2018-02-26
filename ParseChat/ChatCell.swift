6f6d44d40a700296f1d20bd768af7183428488ba
NicoleHipolito <nhipolito1@gmail.com>
2018-02-25 22:33:24 -0800

    can load chat

---
+++ ParseChat/ChatCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChatCell.swift
+//  ParseChat
+//
+//  Created by Nicole Hipolito on 2/25/18.
+//  Copyright © 2018 Nicole Hipolito. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ParseChat/ViewControllers/ChatViewController.swift
+++ ParseChat/ViewControllers/ChatViewController.swift
@@ -9,24 +9,64 @@
 import UIKit
 import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    var messages = [String]()
+    var usernames = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Auto size row height based on cell autolayout constraints
+        tableView.rowHeight = UITableViewAutomaticDimension
+        // Provide an estimated row height. Used for calculating scroll indicator
+        tableView.estimatedRowHeight = 50
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.fetchMessages(_:)), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func fetchMessages(_ sender:Any?){
+        
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        
+        let query = PFQuery(className: "Message")
+        query.includeKey("user")
+        query.order(byDescending: "createdAt")
+        query.findObjectsInBackground { (messages, error) in
+            if(messages != nil){
+                self.messages = []
+                self.usernames = []
+            
+                for message in messages!{
+//                    print(message)
+                    self.messages.append(message["text"] as! String)
+                    if(message["user"] != nil){
+                        self.usernames.append((message["user"] as! PFUser).username!)
+                    }
+                    else{
+                        self.usernames.append("🤖")
+                    }
+                }
+                self.tableView.reloadData()
+            }
+            else{
+                print("Info:! (error?.localizedDescription)")
+            }
+        }
+//        self.refreshControl.endRefreshing()
     }
     
     @IBAction func SendTapped(_ sender: Any) {
         let chatMessage = PFObject(className: "Message")
         chatMessage["text"] = messageField.text ?? ""
+//        chatMessage["user"] = PFUser.current()?.username as! String
+//        print(PFUser.current()?.username as! String)
         chatMessage.saveInBackground { (success, error) in
             if success {
                 print("The message was saved!")
@@ -37,14 +77,24 @@ class ChatViewController: UIViewController {
         messageField.text = ""
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
+        let message = messages[indexPath.row]
+        let username = usernames[indexPath.row]
+        print(message)
+        print(username)
+        cell.messageLabel.text = message
+        cell.userLabel.text = username
+        
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
